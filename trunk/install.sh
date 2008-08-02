66d70756666ac78d1bfe0e5101be4facb2324956
SmSpillaz <SmSpillaz@40c21b38-064d-0410-aec4-33c6a09b6e03>
2008-08-02 02:35:54 +0000

    some-guy: Enhancements to the install script, will add more checks later

--- trunk/install.sh
+++ trunk/install.sh
@@ -2,19 +2,30 @@
 # Simple script to find prefix and install icons. Use --prefix if you installed CCSM in somewhere other than /usr or /usr/local
 
 if [ ! -z `echo $@ | grep -e --prefix=` ]; then
-    PREFIX=`echo $@ | grep -e --prefix= | sed -e s,--prefix=,,`
+	PREFIX=`echo $@ | grep -e --prefix= | sed -e s,--prefix=,,`
 elif [ -d /usr/share/ccsm/icons/hicolor ]; then
-    PREFIX=/usr
+	PREFIX=/usr
 elif [ -d /usr/local/share/ccsm/icons/hicolor ]; then
-    PREFIX=/usr/local
+	PREFIX=/usr/local
 else
-    echo "Can't find prefix, try using the --prefix= option"
+	echo "Can't find prefix, try using the --prefix= option"
 fi
-# copy
-for a in `ls | grep plugin-`; do
-    echo "=> Copying $a"
-    cp $a $PREFIX/share/ccsm/icons/hicolor/scalable/apps/
+
+for app in install gtk-update-icon-cache; do
+	if [ ! -f `which $app` ]; then
+		echo "Can't find $app"
+		echo "Make sure $app is installed and in your \$PATH"
+		exit 3
+	fi
+done 
+
+# install
+for myicon in `ls | grep plugin-`; do
+	echo "* Installing $myicon"
+	install -m 644 $myicon $PREFIX/share/ccsm/icons/hicolor/scalable/apps/
 done
-echo "=> Building gtk icon cache"
+
+# build icon cahce
+echo "* Building gtk icon cache"
 gtk-update-icon-cache -t $PREFIX/share/ccsm/icons/hicolor/
 exit
