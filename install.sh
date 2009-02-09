d7b864757665fa74563161e10b74fa66d8923eae
SmSpillaz <SmSpillaz@40c21b38-064d-0410-aec4-33c6a09b6e03>
2009-02-09 23:37:21 +0000

    **some-guy tango: Moved tango icons to tango/ oxygen: Added some plugins INSTALL: Temporarily set to copy tango then overwrite with oxygen when --oxygen

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Simple script to find prefix and install icons. Use --prefix if you installed CCSM in somewhere other than /usr or /usr/local
 
+if (echo $@ | grep -e --help &>/dev/null); then
+	echo "Options:"
+	echo " --tango \t\t Install with the tango theme [default]"
+	echo " --oxygen \t\t Install with the oxygen theme [incomplete]"
+	echo " --prefix=PREFIX \t CCSM install prefix eg. /usr/local"
+	exit 8
+fi
+
 if [ ! -z `echo $@ | grep -e --prefix=` ]; then
 	PREFIX=`echo $@ | grep -e --prefix= | sed -e s,--prefix=,,`
 elif [ -d /usr/share/ccsm/icons/hicolor ]; then
@@ -20,7 +28,13 @@ for app in install gtk-update-icon-cache; do
 done 
 
 # install
-for myicon in `ls | grep plugin-`; do
+if (echo $@ | grep -e --oxygen &>/dev/null); then
+	LIST=`ls tango | grep plugin- | sed 's|plugin-|tango/plugin-|'`
+	LIST="$LIST `ls oxygen | grep plugin- | sed 's|plugin-|oxygen/plugin-|'`"
+else
+	LIST=`ls tango | grep plugin- | sed 's|plugin-|tango/plugin-|'`
+fi
+for myicon in $LIST; do
 	echo "* Installing $myicon"
 	install -m 644 $myicon $PREFIX/share/ccsm/icons/hicolor/scalable/apps/
 done
