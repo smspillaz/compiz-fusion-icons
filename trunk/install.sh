7b9ea9a6c20bb6583ffe7f855e4345b41d37e78e
SmSpillaz <SmSpillaz@40c21b38-064d-0410-aec4-33c6a09b6e03>
2010-03-19 21:29:29 +0000

    install.sh: Use bashisms and be smarter

--- trunk/install.sh
+++ trunk/install.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 # Simple script to find prefix and install icons. Use --prefix if you installed CCSM in somewhere other than /usr or /usr/local
 
-if (echo $@ | grep -e --help &>/dev/null); then
-	echo -e "Options:"
-	echo -e " --tango \t\t Install with the tango theme [default]"
-	echo -e " --oxygen \t\t Install with the oxygen theme [incomplete]"
-	echo -e " --prefix=PREFIX \t CCSM install prefix eg. /usr/local"
-	exit 8
-fi
+INSTALL_TYPE='tango'
 
-if [ ! -z "`echo $@ | grep -e --prefix=`" ]; then
-	PREFIX="`(for a in $@; do echo $a | grep -e --prefix=; done) | sed -e s,--prefix=,,`"
-elif [ -d /usr/share/ccsm/icons/hicolor ]; then
-	PREFIX=/usr
-elif [ -d /usr/local/share/ccsm/icons/hicolor ]; then
-	PREFIX=/usr/local
-else
-	echo "Can't find prefix, try using the --prefix= option"
+while [[ $1 != "" ]]; do
+	case $1 in
+		--help)
+			echo -e "Options:"
+			echo -e " --tango \t\t Install with the tango theme [default]"
+			echo -e " --oxygen \t\t Install with the oxygen theme [incomplete]"
+			echo -e " --prefix=PREFIX \t CCSM install prefix eg. /usr/local"
+			exit 8
+			;;
+		--prefix=*)
+			PREFIX=$(sed s/--prefix=// <<<$1)
+			;;
+		--oxygen)
+			INSTALL_TYPE='oxygen'
+			;;
+		*)
+			echo "Unknown option $1"
+			;;
+	esac
+	shift
+done
+
+if [[ $PREFIX == "" ]]; then
+	if [[ -d /usr/share/ccsm/icons/hicolor ]]; then
+		PREFIX=/usr
+	elif [[ -d /usr/local/share/ccsm/icons/hicolor ]]; then
+		PREFIX=/usr/local
+	else
+		echo "Can't find prefix, try using the --prefix= option"
+	fi
 fi
 
 for app in install gtk-update-icon-cache; do
-	if [ ! -f `which $app` ]; then
+	if ! which $app &>/dev/null; then
 		echo "Can't find $app"
 		echo "Make sure $app is installed and in your \$PATH"
 		exit 3
@@ -28,10 +44,10 @@ for app in install gtk-update-icon-cache; do
 done 
 
 # install
-if (echo $@ | grep -e --oxygen &>/dev/null); then
-	LIST=`ls oxygen | grep plugin- | sed 's|plugin-|oxygen/plugin-|'`
+if [[ $INSTALL_TYPE == 'oxygen' ]]; then
+	LIST=$(printf "%s\n" oxygen/* | grep 'oxygen/plugin-')
 else
-	LIST=`ls tango | grep plugin- | sed 's|plugin-|tango/plugin-|'`
+	LIST=$(printf "%s\n" tango/* | grep 'tango/plugin-')
 fi
 for myicon in $LIST; do
 	echo "* Installing $myicon"
